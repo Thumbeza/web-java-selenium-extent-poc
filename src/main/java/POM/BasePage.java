684896c7d2d98ad048944304a8b27f00c6f1975e
Siphesihle Hlophe <siphesihle.hlophe@structureit.net>
2022-10-10 07:32:02 +0200

    adding waits for elements

--- src/main/java/POM/BasePage.java
+++ src/main/java/POM/BasePage.java
@@ -2,22 +2,44 @@ package POM;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
     private WebDriver _driver;
+    private WebDriverWait _wait;
+    private static final int WaitInSeconds = 10;
 
     public BasePage(WebDriver driver){
         _driver = driver;
+
+        _wait = new WebDriverWait(_driver, Duration.ofSeconds(WaitInSeconds));
     }
 
     public Boolean Visible;
 
     public Boolean WaitForElementClickable(By by){
-        return true;
+        try{
+            _wait.until(ExpectedConditions.elementToBeClickable(by));
+
+            return true;
+        }
+        catch (Exception exception){
+            return false;
+        }
     }
 
     public Boolean WaitForElementVisible(By by){
-        return true;
+        try{
+            _wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+
+            return true;
+        }
+        catch (Exception exception){
+            return false;
+        }
     }
 
     public void ClickElement(By by){
